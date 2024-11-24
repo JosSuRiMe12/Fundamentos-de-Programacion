b415c7145ac691d7b601a7b9041424c349a10b12
JosSuRiMe12 <jocellyn.sugey1702@gmail.com>
2024-11-24 15:38:01 -0600

    Add files via upload

---
+++ Practica 1/Practica 1 pastel.cpp
@@ -0,0 +1,23 @@
+/*Jocellyn Sugey Rivera Mendoza
+Practica 1- Manejo básico de Salida*/
+
+#include<iostream>
+
+
+using namespace std;
+
+ main()
+ {
+system("color BD");
+    cout<< "     ```````" << endl;
+    cout<< "     |||||||" << endl;
+    cout<< "   @@@@@@@@@@@" << endl;
+    cout<< "  { ~@~ @ ~@~ }" << endl;
+    cout<<" @@@@@@@@@@@@@@@"<< endl;
+    cout<<" {~@  HAPPY  @~}" << endl;
+    cout<< " {   BIRTHDAY  }" << endl;
+    cout<<"@@@@@@@@@@@@@@@@@"<< endl;
+    cout<<"  ___)     (___"<<endl;
+    cout<<" /_____________\adj"<<endl;
+    
+        }
